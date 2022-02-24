920aa89f002f0412f290f9704ed8a25516cf9ae4
Zynh Ludwig <Zynh0722@gmail.com>
2022-02-24 12:31:59 -0800

    Finish functional stack.js`

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,23 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[someInstance.size()] = value;
   };
 
   someInstance.pop = function() {
+    var out = storage[someInstance.size() - 1];
+    delete storage[someInstance.size() - 1];
+    return out;
   };
 
   someInstance.size = function() {
+    var total = 0;
+
+    for (let i in storage) {
+      total++;
+    }
+
+    return total;
   };
 
   return someInstance;
