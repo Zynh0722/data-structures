4cb7398e2814f531207e846b8adca6df8e7a0b3e
Zynh Ludwig <Zynh0722@gmail.com>
2022-02-24 11:46:29 -0800

    Finish Functional Queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,34 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var startIndex = 0;
+  var numValues = 0;
+
 
   // Implement the methods below
 
+  someInstance.size = function() {
+    return numValues;
+  };
+
   someInstance.enqueue = function(value) {
+    storage[startIndex + someInstance.size()] = value;
+    numValues++;
   };
 
   someInstance.dequeue = function() {
-  };
+    var out;
 
-  someInstance.size = function() {
+    if (numValues > 0) {
+      out = storage[startIndex];
+      delete storage[startIndex];
+      startIndex++;
+      numValues--;
+    } else {
+      out = undefined;
+    }
+
+    return out;
   };
 
   return someInstance;
